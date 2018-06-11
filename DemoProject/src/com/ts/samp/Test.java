d5b09f1befe3372d90af361686018599237ac6f0
tsuser <tsuser@TalentSprint>
2018-06-11 10:35:29 +0530

    Country_added_byTL

--- DemoProject/src/com/ts/samp/Test.java
+++ DemoProject/src/com/ts/samp/Test.java
@@ -1,12 +1,21 @@
 package com.ts.samp;
 
-public class Test {
+import java.util.Locale;
 
+public class Test {
 	public static void main(String ar[]) {
 
 		System.out.println("welcome to test");
-		
+	
 		for(int i=1;i<=10;i++)
 			System.out.println("Num is:"+i);
+		
+		String[] locale=Locale.getISOCountries();
+		
+		for(String country:locale) {
+			System.out.println("country is:"+country);
+		}
+		
+		
 	}
 }
