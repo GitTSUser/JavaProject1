261ccc9e7b3c920b49b3ce9bdc787c148bcb97c4
tsuser <tsuser@TalentSprint>
2018-06-11 10:45:04 +0530

    Commit_by_Dev1

--- DemoProject/src/com/ts/samp/Test.java
+++ DemoProject/src/com/ts/samp/Test.java
@@ -4,13 +4,12 @@ import java.util.Locale;
 
 public class Test {
 	public static void main(String ar[]) {
-
 		System.out.println("welcome to test");
 	
 		for(int i=1;i<=10;i++)
 			System.out.println("Num is:"+i);
 		
-		String[] locale=Locale.getISOCountries();
+	String[] locale=Locale.getISOCountries();
 		
 		for(String country:locale) {
 			System.out.println("country is:"+country);
